e2e815d5ee6301396b2ce52a2f9613b145e0115d
Flignats <flignats@gmail.com>
2019-11-14 13:08:46 -0800

    chore(app): lint
    linting
    added en.json es.json
    translation verified working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public navigation = [{ link: 'home', label: 'navigation.home' }];
+    public navigation = [
+        { link: 'home', label: 'navigation.home' },
+        { link: 'settings', label: 'navigation.settings' },
+    ];
     public navigationSideMenu = [...this.navigation];
 }
